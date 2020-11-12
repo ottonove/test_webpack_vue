045c6c9be6454af2baafa302439b178aef255b1e
ottonove <niigata01web@gmail.com>
2020-11-12 16:31:40 +0900

    [WIP] 一時保存。

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,64 @@
 const { resolve } = require('path')
 
 const { VueLoaderPlugin } = require("vue-loader");
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CleanWebpackPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+const devMode = process.env.NODE_ENV !== 'production'
+const srcPath = resolve(__dirname, 'src')
+const distPath = resolve(__dirname, 'dist')
+const bodyPartialFile = resolve(srcPath, 'index.html')
+const wasmFile = resolve(
+  __dirname,
+  'node_modules',
+  'mediainfo.js',
+  'dist',
+  'MediaInfoModule.wasm'
+)
 
 module.exports = {
   // モード値を production に設定すると最適化された状態で、
   // development に設定するとソースマップ有効でJSファイルが出力される
-  mode: "production",
+  mode: devMode ? 'development' : 'production',
+  entry: { app: resolve(srcPath, 'index.js') },
+  plugins: [
+    new VueLoaderPlugin(),
+
+    new HtmlWebpackPlugin({
+      filename: resolve(distPath, 'index.html'),
+      // favicon: resolve(srcPath, 'static', 'favicon.png'),
+      /* meta: {
+        description: pkginfo.description,
+        author: pkginfo.author,
+        keywords: pkginfo.keywords.join(' '),
+        viewport: 'width=device-width,initial-scale=1',
+      }, */
+      // minify: false,
+      // title: 'mediainfo.js',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: wasmFile, to: '.' },
+        // { from: 'CNAME', to: '.' },
+      ],
+    }),
+    new HtmlWebpackPartialsPlugin({ path: bodyPartialFile }),
+    ...(devMode
+      ? [new webpack.HotModuleReplacementPlugin()]
+      : [
+          new CleanWebpackPlugin([distPath]), // clean dist folder before build
+          new MiniCssExtractPlugin({ filename: '[name].css' }),
+        ]),
+  ],
+  output: {
+    path: distPath,
+    // filename: 'mediainfo-demo.js',
+    filename: 'main.js',
+  },
 
   module: {
     rules: [
@@ -39,10 +92,10 @@ module.exports = {
     },
     extensions: ["*", ".js", ".vue", ".json"],
   },
-  plugins: [
+  /* plugins: [
     // Vueを読み込めるようにするため
     new VueLoaderPlugin(),
-  ],
+  ], */
   // ES5(IE11等)向けの指定
   target: ["web", "es5"],
 };
